6c64c587c126354e1cbb4c53fca76ab184e2ff33
MeeedAymen <mohamedaymenazza@gmail.com>
2024-12-04 11:57:20 +0100

    Resources Not Found Exeption

--- src/main/java/com/darkoum/darkoum/repository/UserRepository.java
+++ src/main/java/com/darkoum/darkoum/repository/UserRepository.java
@@ -1,4 +1,16 @@
 package com.darkoum.darkoum.repository;
 
-public class UserRepository {
+import com.darkoum.darkoum.model.User;
+import com.darkoum.darkoum.model.UserRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    List<User> findByRole(UserRole role);
+    boolean existsByEmail(String email);
 }
